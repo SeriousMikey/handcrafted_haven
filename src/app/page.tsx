aa35da3c3dd4ac07960fdfd312ae0a271480b18c
Michael Hyer <michaelhy2002@gmail.com>
2025-03-28 18:14:18 -0600

    Started home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,94 +1,71 @@
+import styles from "./ui/home.module.css";
 import Image from "next/image";
-import styles from "./page.module.css";
 
 export default function Home() {
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
+    <div>
+      <header>
         <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
+          src=""
+          alt=""
+          width={69}
+          height={69}
         />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
+        <h3>Login</h3>
+        <nav>
+          <link>My Profile</link>
+          <link>Catalog</link>
+          <link>About Us</link>
+        </nav>
+      </header>
+      <main>
+        <h1 className={styles.big}>Popular Items</h1>
+        <div>
+          <Image
+            src=""
+            alt=""
+            width={69}
+            height={69}
+          />
             <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
+            src=""
+            alt=""
+            width={69}
+            height={69}
+          />
+            <Image
+            src=""
+            alt=""
+            width={69}
+            height={69}
+          />
         </div>
       </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+      <footer>
+        <div>
+
+        </div>
+          
+        <div>
           <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
+            src=""
+            alt=""
+            width={69}
+            height={69}
           />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
+            <Image
+            src=""
+            alt=""
+            width={69}
+            height={69}
           />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
+            <Image
+            src=""
+            alt=""
+            width={69}
+            height={69}
           />
-          Go to nextjs.org →
-        </a>
+        </div>
       </footer>
     </div>
   );
