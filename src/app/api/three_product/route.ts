cbb12dd6998db50b961fce4e4e13593bf5b7764f
Michael Hyer <michaelhy2002@gmail.com>
2025-04-17 13:57:38 -0600

    changed route

--- src/app/api/three_product/route.ts
+++ src/app/api/three_product/route.ts
@@ -1,23 +1,34 @@
 import { sql } from '@/app/query/sql';
 
-
 export async function GET() {
   try {
     const result = await sql`
-        SELECT 
-            name, description, image
-        FROM 
-            product
-        JOIN 
-            user ON user.id = product.id
-        ORDER BY 
-            product.sales DESC
-        LIMIT 3;
+      SELECT 
+          name, description, image
+      FROM 
+          product
+      JOIN 
+          user ON user.id = product.id
+      ORDER BY 
+          product.sales DESC
+      LIMIT 3;
     `;
 
-    return result;
+    return new Response(JSON.stringify(result), {
+      status: 200,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+
   } catch (err) {
-    console.error('DB insert error:', err);
-    return { status: 500 };
+    console.error('DB query error:', err);
+
+    return new Response(JSON.stringify({ error: 'Internal Server Error' }), {
+      status: 500,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
   }
 }
